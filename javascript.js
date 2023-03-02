f1e1f7503dc9c2982a195089d1b146b5147be704
JanaIsCoding <jana.istrate@gmail.com>
2023-03-02 15:43:00 +0200

    Apply prettier

--- javascript.js
+++ javascript.js
@@ -1,30 +1,33 @@
 function setTheme() {
-    const root = document.documentElement;
-    const newTheme = root.className === 'Dark' ? 'Light' : 'Dark';
-    root.className = newTheme;
-    document.querySelector('.theme-name').textContent = newTheme;
-}
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'Dark') {
-        setTheme('Light');
-    } else {
-        setTheme('Dark');
-    }
+  const root = document.documentElement;
+  const newTheme = root.className === "Dark" ? "Light" : "Dark";
+  root.className = newTheme;
+  document.querySelector(".theme-name").textContent = newTheme;
 }
 
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "Dark") {
+    setTheme("Light");
+  } else {
+    setTheme("Dark");
+  }
+}
 
 const toggleBtn = document.querySelector(".theme-toggle");
-toggleBtn.addEventListener('click', setTheme)
+toggleBtn.addEventListener("click", setTheme);
 
 // API TESTS
-const img = document.querySelector('.api-img')
+const img = document.querySelector(".api-img");
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=qYYK4VN0QiCjprWKsTCA6UPAnHrQEAju&s=peepo', {
-  mode: 'cors'
-})
-.then(function(response) {
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=qYYK4VN0QiCjprWKsTCA6UPAnHrQEAju&s=peepo",
+  {
+    mode: "cors",
+  }
+)
+  .then(function (response) {
     return response.json();
   })
-  .then(function(response) {
+  .then(function (response) {
     img.src = response.data.images.original.url;
   });
